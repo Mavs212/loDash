8c85522423ad48a1dcf4ab8c8cb63d794abf9037
Mavs212 <dbschwartz2@gmail.com>
2015-12-12 20:59:16 -0600

    FinalCommit
    FinalCommit

--- loDash.js
+++ loDash.js
@@ -2,7 +2,9 @@
 
 	var myObj = {
 		name: "dave",
-		location: "dallas",
+		prev_location: "dallas",
+		new_location: "denver",
+		new_school: "galvanize",
 		age: 30,
 
 	}	
@@ -11,23 +13,28 @@
 		console.log(input)
 	}
 
-	print(myArr[1]);
 
-	print(myObj.location);
-	print(myObj['location']);
+	//acessing index of array using bracket notation
+	//print(myArr[1]);    
 
+	//accessing property of object using dot notation
+	//print(myObj.location);
+
+	//accessing prperty of object using bracket notation
+	//print(myObj['location']);
+
+
+	//forEach, which is the heart of the loDash library
 	function forEach(collection, callback) {
 		if (Array.isArray(collection)) {
-			for (var i = 0; i < collection.length; i++) {
-				callback(collection[i]);
-				return collection;
-			}
-		}
+			for (var i = 0; i < collection.length; i++) {	//this if statement checks if the collection parameter is an array, if it is then 
+				callback(collection[i]);					//the loop will implement the callback function
+			}												// through every index  of the array, if the collection parameter is an object 
+		}													//the else statement runs below
 		else {
-			for (var key in collection){
-				if(collection.hasOwnProperty(key))  {
-					callback(collection[i])
-					return collection;
+			for (var key in collection){ 					//Similar to above, except will run if the collection parameter is an object  
+				if(collection.hasOwnProperty(key))  {		//the loop checks to see if the collection has a property, which would only occur if it is an object and if so
+					callback(collection[key])			    //the callback function is executed on the property
 				}
 			}
 		}
@@ -35,38 +42,49 @@
 
 	function map(collection, callback) {
 	 	var result = [];
-	 	forEach(collection, function (element) {
-	    	result.push(callback(element));});
+	 	forEach(collection, function (element) {  //map uses an anonymous callback function is used in the forEach declaration 
+	    	result.push(callback(element));});    //to push the results of callback for each index in the collection array to an empty array called result
 	  return result;
 	}
 
 
 	function filter(collection, predicate) {
-	  var result = [];
-	  forEach(collection, function (element) {
+	  var result = [];							//filter is similar to map, but instead the callback function parameter is now called predicate
+	  forEach(collection, function (element) {   //and the predicate function is now used to determine whether resuts are pushed to the result array.
 	    if (predicate(element))
 	      result.push(element);
 	  });
 	  return result;
 	}
-	 
-
-	function forEach(collection, callback) {
-		if (Array.isArray(collection)) {
-			for (var i = 0; i < collection.length; i++) {
-				callback(collection[i]);}
-		}
-		else {
-			for (var key in collection){
-				if(collection.hasOwnProperty(key))  {
-					callback(collection[key]);}
-		}
-	 }
+	
+	function some(collection, predicate) {   //some is simiar to filter but instead of returning elements that passed the predicate test
+	var bool;								 //this functionr returns true if there are some elements that pass the predicate function test.
+		forEach(collection, function(element) {  //otherwise it returns false.
+			if (predicate(element)) {
+				bool = true;
+			}
+			else {
+				bool = false;
+			}});
+		return bool;
 	}
 
-	function reduce(collection, callback, initial) {
-	  var accumulator = initial;
+
+
+
+	function every(collection, predicate) {   //every is similar to some, except it only returns true if every element in the collection
+	  var result = true;					  //passes the predicate test
 	  forEach(collection, function (element) {
+	    if (!predicate(element)) {
+	      result = false;
+	    }
+	  });
+	  return result;
+	} 
+
+	function reduce(collection, callback, initial) {   //reduce is a function that takes an initial value (if undefined it is the first in the collection)
+	  var accumulator = initial;					   //and adds the result of a callback function on each iorrectlytem in a the collection.
+	  forEach(collection, function (element) {		  //the callback function must have two parameters in order to work c
 	    if (accumulator === undefined){
 	    	accumulator = element;}
 	    accumulator = callback(accumulator, element);
@@ -75,54 +93,35 @@
 	}
 
 
-	function contains(collection, target) {
-	  return reduce(collection, function (accumulator, element) {
-	    if (accumulator) {
-	      return true;
+	function contains(collection, target) {						//contains is similar to some, but it uses reduce to see if an element is present.
+	  return reduce(collection, function (accumulator, element) {     // it also uses a target value instead of a predicate function
+	    if (accumulator) {									
+	      return true;		
 	    }
 	    return element === target;
-	  }, false);
-	}
+	  }, false);													//initial is set to false, so therefore accumulator will equal false unless
+	}																// element === target, aka the current index equals the value of the target
+																	//parameter, otherwise, the function will return true to the end
 
+	
 
-	function some(collection, predicate) {
-	var bool;
-		forEach(collection, function(element) {
-			if(predicate(element)){
-			bool = true;
-			}
-			else {
-			bool = false;
-			}});
-		return bool;
-	}
-
-
-
-
-	function every(collection, predicate) {
-	  var result = true;
-	  forEach(collection, function (element) {
-	    if (!predicate(element)) {
-	      result = false;
-	    }
-	  });
-	  return result;
-	}
-
-	function add(a,b) {
+	function add(a,b) {  //this is an example of a callback function parameter that can be used with the functionk reduce
 		return a + b;
 	}
 
-	function grtThan4(x) {
+	function grtThan4(x) { //this is an example of a callback function that can be used in the functions filter, some, and every
 		return x > 4;
 
 	}
 
 
-	//using the above functions
+	//examples using the above functions
 
 	every(myArr,grtThan4);
 	contains([1,2,3,4,5], 3);
+	reduce([1,2,3,4,5], add);
+	
+	var g19 = ["GoldenTriangle"];
+	contains(g19, "GoldenTriangle");
 
-	reduce(add, 0, [1,2,3,4,5]);
+	
