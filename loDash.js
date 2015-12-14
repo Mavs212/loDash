721bb06ca6afbc78c5bf2a3f877f76616de1c0ff
Mavs212 <dbschwartz2@gmail.com>
2015-12-14 00:45:24 -0800

    CommentsFinalCommitIII
    CommentsFinalCommitIII

--- loDash.js
+++ loDash.js
@@ -27,12 +27,13 @@
 	//forEach, which is the heart of the loDash library
 	function forEach(collection, callback) {
 		if (Array.isArray(collection)) {
-			for (var i = 0; i < collection.length; i++) {//This if statement checks if the collection parameter is an array.
-				callback(collection[i]); // If it is an array is then the loop will implement the callback function
-			}				 // through every index  of the array.  If the collection is an object 
-		}				  	 //the else statement runs below
+			for (var i = 0; i < collection.length; i++) {//This if statement checks if the collection parameter is 
+				callback(collection[i]); // an array.  If it is an array is then the loop will implement 
+			}				 // the callback function through every index  of the array.  
+		}				  	 //If the collection is an object the else statement runs below
 		else {
-			for (var key in collection){     //Similar to above, except will run if the collection parameter is an object  
+			for (var key in collection){     //Similar to above, except will run if the collection
+							//parameter is an object
 				if(collection.hasOwnProperty(key))  {
 					callback(collection[key])			   
 				}
@@ -42,25 +43,26 @@
 
 	function map(collection, callback) {
 	 	var result = [];
-	 	forEach(collection, function (element) {  //map uses an anonymous callback function is used in the forEach declaration 
-	    	result.push(callback(element));});    //to push the results of callback for each index in the collection
-	  return result;			       //array to an empty array called result
-	}
-
+	 	forEach(collection, function (element) {  //map pushes the result of each collection
+	    	result.push(callback(element));});    //item that is run through a callback function to an empty
+	  return result;			       //array.  An anonymous callback function is used to showcase 
+	}					     //that collection[i] in forEach can be passed up to map in the form 
+						     //of element.  Not all parameters in different functions must have the
+						    //the same name.
 
 	function filter(collection, predicate) {
 	  var result = [];			     //filter is similar to map, but instead the callback function parameter 
-	  forEach(collection, function (element) {   //is now called predicateand the predicate function is now used to 
-	    if (predicate(element))		     //determine whether resuts are pushed to the result array.
+	  forEach(collection, function (element) {   //is now called predicate and the elements of the collection will only 
+	    if (predicate(element))		     //be pushed to the result array if they pass the predicate test.
 	      result.push(element);
 	  });
 	  return result;
 	}
 	
 	function some(collection, predicate) {   //some is simiar to filter but instead of returning elements that passed the 
-	var bool;			        //predicate test this functionr returns true if there are some elements 
-		forEach(collection, function(element) {  //that pass the predicate function test. Otherwise it returns false.
-			if (predicate(element)) {
+	var bool;			        //predicate test this function returns one value (true) if there are some 
+		forEach(collection, function(element) {  // elements that pass the predicate function test. 
+			if (predicate(element)) {       //Otherwise it returns false.
 				bool = true;
 			}
 			else {
@@ -83,21 +85,21 @@
 	} 
 
 	function reduce(collection, callback, initial) {   //reduce is a function that takes an initial value 
-	  var accumulator = initial;		          //(if undefined it is the first in the collection)and adds
-	  forEach(collection, function (element) {	// the result of a callback function on each iorrectlytem in a the collection.
-	    if (accumulator === undefined){		//The callback function must have two parameters in order to work properly
-	    	accumulator = element;}
-	    accumulator = callback(accumulator, element);
+	  var accumulator = initial;		          //(if undefined then the first item in the collection)and 
+	  forEach(collection, function (element) {	// performs a callback operation loop that aggregates previous results 
+	    if (accumulator === undefined){		//of array items that were run in the callback function with  
+	    	accumulator = element;}			//a current array item run in the callback function.  What is left 			
+	    accumulator = callback(accumulator, element); //should be a single value for the entire array, aka it is 'reduced'.
 	  });
 	  return accumulator;
 	}
 
 
-	function contains(collection, target) {	//contains is similar to some, but it uses reduce to see if an element is present.
-	  return reduce(collection, function (accumulator, element) {   // It also uses a target value instead of a predicate function.
-	    if (accumulator) {		//initial is set to false, so therefore accumulator will equal false unless							
-	      return true;		// element === target, aka the current index equals the value of the target
-	    }				//parameter, otherwise, the function will return true to the end.
+	function contains(collection, target) {	//contains is similar to some, but it uses reduce to see if an element
+	  return reduce(collection, function (accumulator, element) {   // is present. It also uses a target value 
+	    if (accumulator) {		//instead of a predicate function.  initial is set to false, so 							
+	      return true;		// accumulator will equal false unless element === target, and
+	    }				//then accumulator will forever thereafter be true until the function ends.
 	    return element === target;
 	  }, false);												
 	}															
@@ -109,7 +111,8 @@
 		return a + b;
 	}
 
-	function grtThan4(x) { //this is an example of a callback function that can be used in the functions filter, some, and every
+	function grtThan4(x) { //this is an example of a callback function that can be used in the functions x
+			      //aka the current index equals the value of the target
 		return x > 4;
 
 	}
